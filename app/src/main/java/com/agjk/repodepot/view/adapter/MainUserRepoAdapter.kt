8b09d199b77d6522a0e92ec4b36a0acf27158755
John Le <johnhle7@gmail.com>
2021-02-13 16:31:24 -0500

    Added RecyclerView Adapter

--- app/src/main/java/com/agjk/repodepot/view/adapter/MainUserRepoAdapter.kt
+++ app/src/main/java/com/agjk/repodepot/view/adapter/MainUserRepoAdapter.kt
@@ -3,6 +3,8 @@ package com.agjk.repodepot.view.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.agjk.repodepot.R
 import com.agjk.repodepot.model.data.Repos
@@ -11,16 +13,27 @@ import com.agjk.repodepot.model.data.Users
 class MainUserRepoAdapter(val repoList: List<Repos>) : RecyclerView.Adapter<MainUserRepoAdapter.UserRepoViewHolder>() {
 
     inner class UserRepoViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-
+        val repoDetailCard: ConstraintLayout = itemView.findViewById(R.id.cl_user_item_box)
+        val repoTitle: TextView = itemView.findViewById(R.id.tv_repo_name)
+        val repoDescription: TextView = itemView.findViewById(R.id.tv_repo_description)
+        val repoLanguage: TextView = itemView.findViewById(R.id.tv_repo_language)
+        val repoRating: TextView = itemView.findViewById(R.id.tv_rating_count)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserRepoViewHolder {
         return UserRepoViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_users, parent, false))
     }
 
-    override fun getItemCount(): Int = 0
+    override fun getItemCount(): Int = repoList.size
 
     override fun onBindViewHolder(holder: UserRepoViewHolder, position: Int) {
+        val repo = repoList[position]
 
+        holder.apply {
+            repoTitle.text = repo.repoName
+            repoDescription.text = repo.repoDescription
+            repoLanguage.text = repo.repoLanguage
+            repoRating.text = repo.repoStarNum.toString()
+        }
     }
 }
