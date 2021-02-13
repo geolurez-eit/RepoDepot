3614ed6194c5aee08437577f0e3729524bedea3e
geolurez-eit <76801116+geolurez-eit@users.noreply.github.com>
2021-02-12 20:22:42 -0500

    Added Retrofit dependencies Added Constants to store url/paths Added GitApi for Retrofit

---
+++ app/src/main/java/com/agjk/repodepot/network/GitApi.kt
@@ -0,0 +1,13 @@
+package com.agjk.repodepot.network
+
+import com.agjk.repodepot.util.Constants
+import com.agjk.repodepot.util.Constants.Companion.URL_PATH
+import com.agjk.repodepot.util.Constants.Companion.USER_NAME_PATH
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface GitApi {
+    //@GET(URL_PATH)
+    //fun getGitResponse(@Path(USER_NAME_PATH) userName: String?): Call<List<GitResponse?>?>?
+}
---
+++ app/src/main/java/com/agjk/repodepot/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.agjk.repodepot.util
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://api.github.com/"
+        const val USER_NAME_PATH = "username"
+        const val URL_PATH = "/users/{$USER_NAME_PATH}/repos"
+        const val TAG_X = "TAG_X"
+    }
+}
