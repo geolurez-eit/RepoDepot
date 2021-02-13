e35adc8f3a44efa29002e6c13e24a6f8ed790400
Adam Ormsby <adam.ormsby@enhanceit.us>
2021-02-13 18:38:22 -0500

    Update login page to check for current user, log in

--- app/src/main/java/com/agjk/repodepot/SplashScreen.kt
+++ app/src/main/java/com/agjk/repodepot/SplashScreen.kt
@@ -1,56 +1,131 @@
 package com.agjk.repodepot
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.view.View
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.agjk.repodepot.util.DebugLogger
+import com.agjk.repodepot.view.MainActivity
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.android.gms.tasks.Task
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.OAuthProvider
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashScreen : AppCompatActivity() {
 
-    private val SPLASH_TIME: Long = 3000
+    private val firebaseAuth = FirebaseAuth.getInstance()
+    private var provider = OAuthProvider.newBuilder("github.com")
+
+    private val SPLASH_TIME: Long = 2000
 
     private lateinit var sloganTextView: TextView
     private lateinit var logoImageView: ImageView
     private lateinit var loginbtn: FloatingActionButton
-
+    private lateinit var loginText: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_screen_activity)
 
-    /*    Handler().postDelayed({
-            //startActivity(Intent(this, MainActivity::class.java))
-            finish()
-        }
-        , SPLASH_TIME)*/
-
+        // Target specific email with login hint.
+        provider.addCustomParameter("login", "george.perez@enhanceit.us")
+        // Request read access to a user's email addresses.
+        // This must be preconfigured in the app's API permissions.
+        val scopes: List<String> = listOf("user", "repo:status")
+        provider.scopes = scopes
 
         sloganTextView= findViewById(R.id.slogan)
         logoImageView = findViewById(R.id.RepotDepotLogo)
         loginbtn = findViewById(R.id.sign_in_Button_main)
+        loginText = findViewById(R.id.login_text)
 
         // Bounce animation for the slogan (textview)
-        logoImageView.visibility = View.VISIBLE
         val animationRotate = AnimationUtils.loadAnimation(this, R.anim.rotate_text)
         logoImageView.startAnimation(animationRotate)
 
         // Bounce animation for the slogan (textview)
-        sloganTextView.visibility = View.VISIBLE
         val animationFadeIn = AnimationUtils.loadAnimation(this, R.anim.zoom_in_text)
         sloganTextView.startAnimation(animationFadeIn)
 
-        // Animation for the login button
+        // only run button animation if no use logged int
+        firebaseAuth.currentUser?.let {
+            loginbtn.visibility = View.INVISIBLE
+            loginText.visibility = View.INVISIBLE
+        } ?: {
+            // Animation for the login button
+            val animationSlideDown = AnimationUtils.loadAnimation(this, R.anim.slide_down_text)
+            loginbtn.startAnimation(animationSlideDown)
 
-        loginbtn.visibility = View.VISIBLE
-        val animationSlideDown = AnimationUtils.loadAnimation(this, R.anim.slide_down_text)
-        loginbtn.startAnimation(animationSlideDown)
+            loginbtn.setOnClickListener {
+                //Check if login is pending, sign in if not
+                checkPendingResult()
+            }
+        }()
+    }
 
+    override fun onStart() {
+        super.onStart()
 
+        firebaseAuth.currentUser?.let {
+            GlobalScope.launch {
+                delay(SPLASH_TIME)
+                startMainActivity()
+            }
+        }
+    }
+
+    private fun startMainActivity() {
+        startActivity(Intent(this, MainActivity::class.java).also { i ->
+            i.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        })
+    }
+
+    private fun startSignIn() {
+        firebaseAuth
+            .startActivityForSignInWithProvider( /* activity= */this, provider.build())
+            .addOnSuccessListener {
+                // User is signed in.
+                // IdP data available in
+                // authResult.getAdditionalUserInfo().getProfile().
+                // The OAuth access token can also be retrieved:
+                // authResult.getCredential().getAccessToken().
+                DebugLogger(it.credential?.provider.toString())
+            }
+            .addOnFailureListener {
+                // Handle failure.
+                DebugLogger(it.localizedMessage)
+            }
+    }
+
+    private fun checkPendingResult() {
+        val pendingResultTask: Task<AuthResult>? = firebaseAuth.pendingAuthResult
+        if (pendingResultTask != null) {
+            // There's something already here! Finish the sign-in for your user.
+            pendingResultTask
+                .addOnSuccessListener(
+                    OnSuccessListener {
+                        // User is signed in.
+                        // IdP data available in
+                        // authResult.getAdditionalUserInfo().getProfile().
+                        // The OAuth access token can also be retrieved:
+                        // authResult.getCredential().getAccessToken().
+                    })
+                .addOnFailureListener {
+                    // Handle failure.
+                }
+        } else {
+            // There's no pending result so you need to start the sign-in flow.
+            // See below.
+            startSignIn()
+        }
 
     }
 }
--- app/src/main/java/com/agjk/repodepot/view/MainActivity.kt
+++ app/src/main/java/com/agjk/repodepot/view/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.agjk.repodepot.view
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
@@ -12,24 +11,17 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
-import com.agjk.repodepot.util.DebugLogger
 import com.agjk.repodepot.R
-import com.agjk.repodepot.model.data.Users
+import com.agjk.repodepot.util.DebugLogger
 import com.agjk.repodepot.view.adapter.MainFragmentAdapter
 import com.agjk.repodepot.view.adapter.UserAdapter
 import com.agjk.repodepot.viewmodel.RepoViewModel
 import com.agjk.repodepot.viewmodel.RepoViewModelFactory
-import com.google.android.gms.tasks.OnSuccessListener
-import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.OAuthProvider
 
 class MainActivity : AppCompatActivity() {
     private val firebaseAuth = FirebaseAuth.getInstance()
-    private var provider = OAuthProvider.newBuilder("github.com")
 
-    private lateinit var mainTextView: TextView
     private lateinit var navigationDrawer: DrawerLayout
     private lateinit var userRecyclerView: RecyclerView
     private lateinit var viewPager: ViewPager2
@@ -43,7 +35,6 @@ class MainActivity : AppCompatActivity() {
     private val repoViewModel: RepoViewModel by viewModels(
         factoryProducer = { RepoViewModelFactory }
     )
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         DebugLogger("MainActivity onCreate")
@@ -64,16 +55,6 @@ class MainActivity : AppCompatActivity() {
         )*/
 
 //        userAdapter.updateUsers(testList)
-
-        // Target specific email with login hint.
-        provider.addCustomParameter("login", "george.perez@enhanceit.us")
-        // Request read access to a user's email addresses.
-        // This must be preconfigured in the app's API permissions.
-        val scopes: List<String> = listOf("user", "repo:status")
-        provider.scopes = scopes
-
-        //Check if login is pending, sign in if not
-        //checkPendingResult()
 
         //Testing viewmodel methods
         DebugLogger("MainActivity onCreate - saveNewRepos")
@@ -138,51 +119,5 @@ class MainActivity : AppCompatActivity() {
         } else {
             super.onBackPressed()
         }
-
-
     }
-
-    private fun startSignIn() {
-        firebaseAuth
-            .startActivityForSignInWithProvider( /* activity= */this, provider.build())
-            .addOnSuccessListener {
-                // User is signed in.
-                // IdP data available in
-                // authResult.getAdditionalUserInfo().getProfile().
-                // The OAuth access token can also be retrieved:
-                // authResult.getCredential().getAccessToken().
-                DebugLogger(it.credential?.provider.toString())
-                mainTextView.text = it.credential?.provider.toString()
-            }
-            .addOnFailureListener {
-                // Handle failure.
-                DebugLogger(it.localizedMessage)
-            }
-    }
-
-
-    private fun checkPendingResult() {
-        val pendingResultTask: Task<AuthResult>? = firebaseAuth.pendingAuthResult
-        if (pendingResultTask != null) {
-            // There's something already here! Finish the sign-in for your user.
-            pendingResultTask
-                .addOnSuccessListener(
-                    OnSuccessListener {
-                        // User is signed in.
-                        // IdP data available in
-                        // authResult.getAdditionalUserInfo().getProfile().
-                        // The OAuth access token can also be retrieved:
-                        // authResult.getCredential().getAccessToken().
-                    })
-                .addOnFailureListener {
-                    // Handle failure.
-                }
-        } else {
-            // There's no pending result so you need to start the sign-in flow.
-            // See below.
-            startSignIn()
-        }
-
-    }
-
 }
