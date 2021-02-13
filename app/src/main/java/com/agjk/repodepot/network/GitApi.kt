b19e3ec6f14edb1d8746f43578d68e495bfc9617
geolurez-eit <76801116+geolurez-eit@users.noreply.github.com>
2021-02-12 22:02:22 -0500

    Added more to Constants.kt and additional get methods in Api

--- app/src/main/java/com/agjk/repodepot/network/GitApi.kt
+++ app/src/main/java/com/agjk/repodepot/network/GitApi.kt
@@ -1,13 +1,17 @@
 package com.agjk.repodepot.network
 
 import com.agjk.repodepot.util.Constants
-import com.agjk.repodepot.util.Constants.Companion.URL_PATH
+import com.agjk.repodepot.util.Constants.Companion.URL_PATH_COMMITS
+import com.agjk.repodepot.util.Constants.Companion.URL_PATH_REPOS
 import com.agjk.repodepot.util.Constants.Companion.USER_NAME_PATH
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface GitApi {
-    //@GET(URL_PATH)
-    //fun getGitResponse(@Path(USER_NAME_PATH) userName: String?): Call<List<GitResponse?>?>?
+    @GET(URL_PATH_REPOS)
+    fun getGitRepos(@Path(USER_NAME_PATH) userName: String?)
+
+    @GET(URL_PATH_COMMITS)
+    fun getGitRepoCommits(@Path(USER_NAME_PATH) userName: String?)
 }
--- app/src/main/java/com/agjk/repodepot/util/Constants.kt
+++ app/src/main/java/com/agjk/repodepot/util/Constants.kt
@@ -4,7 +4,9 @@ class Constants {
     companion object {
         const val BASE_URL = "https://api.github.com/"
         const val USER_NAME_PATH = "username"
-        const val URL_PATH = "/users/{$USER_NAME_PATH}/repos"
+        const val REPO_PATH = "repo"
+        const val URL_PATH_REPOS = "/users/{$USER_NAME_PATH}/repos"
+        const val URL_PATH_COMMITS = "/repos/{$USER_NAME_PATH}/${REPO_PATH}/commits"
         const val TAG_X = "TAG_X"
     }
 }
