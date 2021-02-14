ddcdc1b7a617d03d3291d5d036170b46316393d5
Adam Ormsby <adam.ormsby@enhanceit.us>
2021-02-13 22:11:14 -0500

    Update element show/hide events on login

--- app/src/main/java/com/agjk/repodepot/SplashScreen.kt
+++ app/src/main/java/com/agjk/repodepot/SplashScreen.kt
@@ -2,9 +2,11 @@ package com.agjk.repodepot
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.agjk.repodepot.util.DebugLogger
@@ -24,12 +26,13 @@ class SplashScreen : AppCompatActivity() {
     private val firebaseAuth = FirebaseAuth.getInstance()
     private var provider = OAuthProvider.newBuilder("github.com")
 
-    private val SPLASH_TIME: Long = 2000
+    private val SPLASH_TIME: Long = 1000
 
     private lateinit var sloganTextView: TextView
     private lateinit var logoImageView: ImageView
     private lateinit var loginbtn: FloatingActionButton
     private lateinit var loginText: TextView
+    private lateinit var progressBar: ProgressBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,21 +49,26 @@ class SplashScreen : AppCompatActivity() {
         logoImageView = findViewById(R.id.RepotDepotLogo)
         loginbtn = findViewById(R.id.sign_in_Button_main)
         loginText = findViewById(R.id.login_text)
+        progressBar = findViewById(R.id.progress_bar)
 
         // Bounce animation for the slogan (textview)
+        logoImageView.visibility = View.VISIBLE
         val animationRotate = AnimationUtils.loadAnimation(this, R.anim.rotate_text)
         logoImageView.startAnimation(animationRotate)
 
         // Bounce animation for the slogan (textview)
-        val animationFadeIn = AnimationUtils.loadAnimation(this, R.anim.zoom_in_text)
-        sloganTextView.startAnimation(animationFadeIn)
+        sloganTextView.visibility = View.VISIBLE
+        val animationZoomIn = AnimationUtils.loadAnimation(this, R.anim.zoom_in_text)
+        sloganTextView.startAnimation(animationZoomIn)
 
-        // only run button animation if no use logged int
+        // only run button animation if no user logged in
         firebaseAuth.currentUser?.let {
-            loginbtn.visibility = View.INVISIBLE
-            loginText.visibility = View.INVISIBLE
+            startMainActivity()
         } ?: {
+            progressBar.visibility = View.INVISIBLE
+
             // Animation for the login button
+            loginbtn.visibility = View.VISIBLE
             val animationSlideDown = AnimationUtils.loadAnimation(this, R.anim.slide_down_text)
             loginbtn.startAnimation(animationSlideDown)
 
@@ -71,21 +79,26 @@ class SplashScreen : AppCompatActivity() {
         }()
     }
 
-    override fun onStart() {
-        super.onStart()
+    private fun startMainActivity() {
+        Log.d("TAG_A", "starting main activity")
 
-        firebaseAuth.currentUser?.let {
-            GlobalScope.launch {
-                delay(SPLASH_TIME)
-                startMainActivity()
+        val animFadeOut = AnimationUtils.loadAnimation(this, R.anim.fast_fade_out)
+        loginbtn.visibility = View.INVISIBLE
+        loginbtn.startAnimation(animFadeOut)
+        loginText.visibility = View.INVISIBLE
+        loginText.startAnimation(animFadeOut)
+
+        progressBar.visibility = View.VISIBLE
+        val animFadeIn = AnimationUtils.loadAnimation(this, R.anim.fast_fade_in)
+        progressBar.startAnimation(animFadeIn)
+
+        GlobalScope.launch {
+            delay(SPLASH_TIME)
+            runOnUiThread {
+                startActivity(Intent(this@SplashScreen, MainActivity::class.java))
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
             }
         }
-    }
-
-    private fun startMainActivity() {
-        startActivity(Intent(this, MainActivity::class.java).also { i ->
-            i.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-        })
     }
 
     private fun startSignIn() {
@@ -98,10 +111,13 @@ class SplashScreen : AppCompatActivity() {
                 // The OAuth access token can also be retrieved:
                 // authResult.getCredential().getAccessToken().
                 DebugLogger(it.credential?.provider.toString())
+                Log.d("TAG_A", "sign in success")
+                startMainActivity()
             }
             .addOnFailureListener {
                 // Handle failure.
                 DebugLogger(it.localizedMessage)
+                Log.d("TAG_A", "sign in failure -> ${it.localizedMessage}")
             }
     }
 
@@ -117,13 +133,17 @@ class SplashScreen : AppCompatActivity() {
                         // authResult.getAdditionalUserInfo().getProfile().
                         // The OAuth access token can also be retrieved:
                         // authResult.getCredential().getAccessToken().
+                        Log.d("TAG_A", "pending result success")
+                        startMainActivity()
                     })
                 .addOnFailureListener {
                     // Handle failure.
+                    Log.d("TAG_A", "pending result failure")
                 }
         } else {
             // There's no pending result so you need to start the sign-in flow.
             // See below.
+            Log.d("TAG_A", "pending result is null, start sign in")
             startSignIn()
         }
 
