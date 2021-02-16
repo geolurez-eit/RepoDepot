55eb3373b71b305ce4991a5d485d43368e0afc94
John Le <johnhle7@gmail.com>
2021-02-16 02:07:40 -0500

    Change repo data class

--- app/src/main/java/com/agjk/repodepot/model/data/Repos.kt
+++ app/src/main/java/com/agjk/repodepot/model/data/Repos.kt
@@ -2,6 +2,11 @@ package com.agjk.repodepot.model.data
 
 data class Repos (
     val repoName: String,
-    val repoDescription: String,
+
     val repoLanguage: String,
+    val repoImageUrl: String,
+    val repoLogin: String,
+
+    val repoStarGazer: String
+
 )
